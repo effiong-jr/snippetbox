eda6f0d89b180f948bca89213f12e2fe2eb783d4
Effiong Bassey <effiong.bassey@arm.com.ng>
2024-03-17 14:02:12 +0100

    Integrated a DB connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/effiong-jr/snippetbox/internal/models"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type application struct {
 	logInfo  *log.Logger
 	logError *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
+
+	dbConnectionString := "postgres://web:54321@localhost:5432/snippetbox"
+
+	dbPool, err := pgxpool.New(context.Background(), dbConnectionString)
+
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Unable to create connection pool: %v", err)
+		os.Exit(1)
+	}
+
+	defer dbPool.Close()
 
 	logInfo := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
@@ -21,6 +39,7 @@ func main() {
 	app := &application{
 		logInfo:  logInfo,
 		logError: logError,
+		snippets: &models.SnippetModel{DB: dbPool},
 	}
 
 	addr := flag.String("addr", ":4000", "HTTP server address")
@@ -33,7 +52,7 @@ func main() {
 
 	logInfo.Printf("Server started on port %s", *addr)
 
-	err := http.ListenAndServe(*addr, mux)
+	err = http.ListenAndServe(*addr, mux)
 
 	logError.Fatal(err)
 
